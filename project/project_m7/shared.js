daa9996d09c5d5adf4601bafb8a58c217a0c3f32
daniel.arellano <danarellanog@gmail.com>
2023-02-02 16:43:04 -0600

    using js

---
+++ project/project_m7/shared.js
@@ -0,0 +1,35 @@
+var backdrop = document.querySelector('.backdrop');
+var modal = document.querySelector('.modal');
+var modalNoButton = document.querySelector(".modal__action--negative");
+var selectPlanButtons = document.querySelectorAll('.plan button');
+var toggleButton = document.querySelector(".toggle-button");
+var mobileNav = document.querySelector(".mobile-nav");
+
+console.dir(backdrop);
+console.dir(selectPlanButtons);
+console.dir(modal);
+
+// backdrop.style.display = "block";
+for (var i = 0;i<selectPlanButtons.length;i++){
+    selectPlanButtons[i].addEventListener('click', function() {
+        backdrop.style.display = 'block';
+        modal.style.display = 'block';
+    });
+}
+
+backdrop.addEventListener("click", function() {
+    mobileNav.style.display = "none";
+    closeModal(); 
+});
+
+modalNoButton.addEventListener("click",closeModal);
+
+function closeModal(){
+    backdrop.style.display = "none";
+    modal.style.display = "none";
+}
+
+toggleButton.addEventListener("click", function(){
+    mobileNav.style.display = "block";
+    backdrop.style.display = "block";
+});
